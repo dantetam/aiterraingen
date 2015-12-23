64d70c84980a3b8f5ca607016f9888b4fff56c05
dantetam <dante.tam1@gmail.com>
2015-12-22 22:41:50 -0800

    Worker AI method

--- src/level/Intelligence.java
+++ src/level/Intelligence.java
@@ -14,24 +14,44 @@ public class Intelligence {
 			for (int cc = t.col - 2; cc <= t.col + 2; cc++)
 				if (en.location.grid.getTile(rr,cc) != null)
 					score += en.location.grid.evalTile(en.owner, rr, cc);
-		return (int)(score/turnsSettlerToTile(en,t));
+		return (int)(score/turnsEntityToTile(en,t));
 	}
-	
-	public double turnsSettlerToTile(BaseEntity en, Tile t)
+
+	public double turnsEntityToTile(BaseEntity en, Tile t)
 	{
 		ArrayList<Tile> path = en.location.grid.findPath(en, en.location.row, en.location.col, t.row, t.col);
 		return Math.ceil((double)path.size()/(double)en.maxAction);
 	}
-	
+
 	public double scoreFromWorkerInTurns(BaseEntity en, int turns)
 	{
+		ArrayList<Tile> queueToImpr = new ArrayList<Tile>();
+		int score = 0;
 		while (true)
 		{
 			if (turns <= 0) break;
-			for (int rr = t.row - 2; rr <= t.row + 2; rr++)
-				for (int cc = t.col - 2; cc <= t.col + 2; cc++)
-					if (en.location.grid.getTile(rr,cc) != null)
+			int turnsBest = -1; double gainPerTurn = 0;
+			for (int rr = en.location.row - 2; rr <= en.location.row + 2; rr++)
+				for (int cc = en.location.col - 2; cc <= en.location.col + 2; cc++)
+				{
+					Tile t = en.location.grid.getTile(rr,cc);
+					if (t.foodImpr > 0 || t.metalImpr > 0 || queueToImpr.contains(t)) continue; //Skip if improved or about to be improved
+					if (t != null)
+					{
+						int turnsCandidate = (int)turnsEntityToTile(en, t);
+						int[] impr = Tile.getImprovement(t.biome, t.resource);
+						double gainPerTurnCandidate = (impr[0]+impr[1])/turnsCandidate;
+						if (gainPerTurnCandidate > gainPerTurn || turnsBest == -1)
+						{
+							gainPerTurn = gainPerTurnCandidate;
+							turnsBest = turnsCandidate;
+						}
+					}
+				}
+			turns -= turnsBest;
+			score += gainPerTurn;
 		}
+		return score; //Is already divided by turns
 	}
-	
+
 }
