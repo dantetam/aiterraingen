26668c3e0298ff5d036cfc6b8a7fc39f1c412316
dantetam <dante.tam1@gmail.com>
2016-01-05 12:08:12 -0800

    Depth traversal in minimax

--- src/level/MinimaxTree.java
+++ src/level/MinimaxTree.java
@@ -4,8 +4,39 @@ import java.util.ArrayList;
 
 public class MinimaxTree {
 
-	public Node first;
+	public Node first = new Node(NodeType.MIN, 0);
 
+	public static void main(String[] args)
+	{
+		int levelsBelow = 5;
+		for (int i = 0; i < levelsBelow; i++)
+		{
+			//Find node of level i
+			ArrayList<Node> nodes = findNodesDepth(0); Remember to commit code on day of 1/5/2016
+			populateNodeChildren(i,-i);
+		}
+	}
+	
+	//Depth traversal
+	public ArrayList<Node> findNodesDepth(ArrayList<Node> nodes, int depth)
+	{
+		if (nodes == null)
+		{
+			nodes = new ArrayList<Node>();
+			nodes.add(first);
+		}
+		if (depth == 0)
+			return nodes;
+		else
+		{
+			ArrayList<Node> newNodes = new ArrayList<Node>();
+			for (Node node: nodes)
+				for (Node n: node.children)
+					newNodes.add(n);
+			return findNodesDepth(newNodes, depth-1); //--depth doesn't look as good
+		}
+	}
+	
 	public class Node
 	{
 		public NodeType type;
