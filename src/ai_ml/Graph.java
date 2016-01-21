23bc157660e2949985899810164bdfcf5827fe30
dantetam <dante.tam1@gmail.com>
2016-01-20 21:40:12 -0800

    Small changes

--- src/ai_ml/Graph.java
+++ src/ai_ml/Graph.java
@@ -1,7 +1,9 @@
 package ai_ml;
 
+import java.util.ArrayList;
+
 public class Graph extends ProtectedGridP<Position> {
-	
+
 	public Graph(int rows, int cols)
 	{
 		tiles = new Position[rows][cols];
@@ -20,19 +22,53 @@ public class Graph extends ProtectedGridP<Position> {
 		}
 		determineTileBias();
 	}
-	
+
 	public void determineTileBias()
 	{
+		ArrayList<GraphEntity> entities = new ArrayList<GraphEntity>();
+		ArrayList<Double> distances = new ArrayList<Double>();
+		int limit = 10;
 		for (int r = 0; r < rows; r++)
-		{
 			for (int c = 0; c < cols; c++)
 			{
 				Position pos = getTile(r,c);
-				Implement k-NN here
+				if (pos.occupant != null)
+					entities.add(pos.occupant);
+			}
+		for (int row = 0; row < rows; row++)
+		{
+			for (int col = 0; col < cols; col++)
+			{
+				Position center = getTile(row,col);
+				int blue = 0, orange = 0;
+				/*for (int r = 0; r < rows; r++)
+				{
+					//Implement k-NN here
+					int i = 0;
+					if (distances.get(i) < )	
+				}*/
+				for (GraphEntity en: entities)
+				{
+					double dist = en.pos.dist(center);
+					for (int i = 0; i < limit; i++)
+					{
+						if (i >= distances.size() || dist < distances.get(i))
+						{
+							distances.add(i, dist);
+							break;
+						}
+					}
+				}
 			}
 		}
 	}
-	
+	public class EntityDistLink 
+	{
+		public GraphEntity en;
+		public double dist;
+		public EntityDistLink(GraphEntity e, double d) {en = e; dist = d;}
+	}
+
 	public void move(GraphEntity en, Position p)
 	{
 		if (p.occupant != null) return;
@@ -50,5 +86,5 @@ public class Graph extends ProtectedGridP<Position> {
 			}
 		}
 	}
-	
+
 }
